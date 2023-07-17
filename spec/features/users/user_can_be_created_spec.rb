8b497a5bbfdcb6d5ad4655489c8b29f05d429be7
Lauren Golden <llgolden@gmail.com>
2023-07-17 11:35:32 -0600

    Classwork

---
+++ spec/features/users/user_can_be_created_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+# As a visitor
+# When I visit '/'
+# and I can click a link that says "Register as a User"
+# and I can enter registration details in a form
+# and submit that form
+# Then I should see a welcome message with my username
+# and my user details have been saved in the database.
+
+RSpec.describe "User registration form" do
+  it "creates new user" do
+    visit root_path
+
+    click_on "Register as a User"
+
+    expect(current_path).to eq(new_user_path)
+
+    username = "funbucket13"
+    password = "test"
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: password
+
+    click_on "Create User"
+
+    expect(page).to have_content("Welcome, #{username}!")
+  end
+end
