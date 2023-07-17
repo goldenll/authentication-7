23cafb7ab93240010da82d0129b2fa4e94066aea
Lauren Golden <llgolden@gmail.com>
2023-07-17 11:47:34 -0600

    Complete Intro to Basic Authentication class lesson

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(username: params[:username])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.username}!"
       redirect_to root_path
     else 
